28b8db859c37e3149950fa74ec6ac5107a76ea3d
Florian LEBOUL <florianleboul@gmail.com>
2021-01-04 21:09:47 +0100

    Seb update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -34,7 +34,7 @@ class User implements UserInterface
     private $pseudo;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      * @Assert\Email()
      */
     private $mailAddress;
