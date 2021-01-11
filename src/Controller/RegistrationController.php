88db918e9881630fa98eff996fa62765d51b094a
florianleboul@gmail.com <florianleboul@gmail.com>
2021-01-11 09:59:14 +0100

    Google mailer installed

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -80,8 +80,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        //$this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('home');
     }
 }
